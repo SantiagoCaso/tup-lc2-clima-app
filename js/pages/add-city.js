5ae401fdfa80dc886b199d1a319265a391743934
SantiagoCaso <santiagoignaciocaso@gmail.com>
2022-06-22 15:20:30 -0300

    add-city terminado, carteles verde/amarillo/rojo, y validación si la ciudad nueva está o no en el localStorage

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,1 +1,47 @@
+let newCity = ' ';
+const cartelVerde = document.querySelector(".mensaje.green")
+const cartelRojo = document.querySelector(".mensaje.red")
+const cartelAmarillo = document.querySelector(".mensaje.yellow")
+
+function agregarNombreCiudad(letCiudad){
+    let ciudad = document.getElementById("add-city").value;
+    newCity = ciudad;
+    letCiudad = ciudad;
+    return letCiudad;
+}
+
+
+function getCitiesFromLocalStorage() {
+    let cities = localStorage.getItem("CITIES");
+    if (cities) {
+        cities = JSON.parse(cities);
+    } else {
+        cities = [];
+    }
+    return cities;
+}
+
+
+function addNewCityToLocalStorage(newCity) {
+    let cities = getCitiesFromLocalStorage();  
+    if (cities.indexOf(newCity) === -1) {
+        cities.push(newCity);
+        localStorage.setItem("CITIES", JSON.stringify(cities));
+        console.log("Se añadio la nueva ciudad con exito");
+        cartelAmarillo.removeAttribute("style");
+        cartelAmarillo.setAttribute("style", "display: none");
+        cartelVerde.removeAttribute("style");
+        cartelVerde.setAttribute("style", "display: flex");
+    }  else if ( cities.indexOf(newCity) > -1) {
+        cartelVerde.removeAttribute("style");
+        cartelVerde.setAttribute("style", "display: none");
+        cartelAmarillo.removeAttribute("style");
+        cartelAmarillo.setAttribute("style", "display: flex");
+        console.log(newCity + 'Ya existe dentro del array de CITIES del localStorage');
+    }else{
+        console.log("Huvo un error al añadir la nueva ciudad");
+        cartelRojo.removeAttribute("style");
+        cartelRojo.setAttribute("style", "display: flex");
+    }
+}
 
--- js/pages/index.js
+++ js/pages/index.js
@@ -3,3 +3,12 @@ function showCard(){
     carta.removeAttribute("style");
     carta.setAttribute("style", "display: block");
 }
+
+const selctCities = document.getElementById("city-list");
+
+function showCitiesSelect(newCity){
+    let option = document.createElement('option');
+    let valor = newCity;
+    option.text= valor;
+    selctCities.appendChild(option);
+}
