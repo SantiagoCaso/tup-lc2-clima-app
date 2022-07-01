0748b7c744a93392859b2d2844350a49bde9fe4f
SantiagoCaso <santiagoignaciocaso@gmail.com>
2022-07-01 12:53:07 -0300

    Correccion N°1 del TP: -Arreglar diferencias entre mayusculas y minusculas (también sin espacios)

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -3,15 +3,23 @@ const cartelVerde = document.querySelector(".mensaje.green")
 const cartelRojo = document.querySelector(".mensaje.red")
 const cartelAmarillo = document.querySelector(".mensaje.yellow")
 
-function agregarNombreCiudad(letCiudad){
-    let ciudad = document.getElementById("add-city").value;
-    newCity = ciudad;
-    letCiudad = ciudad;
-    return letCiudad;
-}
 
-function addNewCityToLocalStorage(newCity) {
-    let cities = getCitiesFromLocalStorage();  
+
+function addNewCityToLocalStorage() {
+    let ciudad = document.getElementById("add-city").value;
+    // registra la ciudad con mayusculas gracias a .toUpperCase() y sin espacios al final o adelante gracias a .trimp() 
+    newCity = ciudad.toUpperCase().trim();
+    
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + newCity + '&appid=c4d912fb3ee953d0623afe3268b33688&units=metric&lang=es')
+        .then(response => response.json())
+        .then(datos => {
+            let ID = datos.id;
+            console.log(ID);
+            return ID;
+        })
+        .catch(error => alert("La API no funcionó"));
+    
+    let cities = getCitiesFromLocalStorage();
     if (cities.indexOf(newCity) === -1) {
         cities.push(newCity);
         localStorage.setItem("CITIES", JSON.stringify(cities));
@@ -20,16 +28,18 @@ function addNewCityToLocalStorage(newCity) {
         cartelAmarillo.setAttribute("style", "display: none");
         cartelVerde.removeAttribute("style");
         cartelVerde.setAttribute("style", "display: flex");
-    }  else if ( cities.indexOf(newCity) > -1) {
+    } else if (cities.indexOf(newCity) > -1) {
         cartelVerde.removeAttribute("style");
         cartelVerde.setAttribute("style", "display: none");
         cartelAmarillo.removeAttribute("style");
         cartelAmarillo.setAttribute("style", "display: flex");
         console.log(newCity + ' Ya existe dentro del array de CITIES del localStorage');
-    }else{
+    } else {
         console.log("Huvo un error al añadir la nueva ciudad");
         cartelRojo.removeAttribute("style");
         cartelRojo.setAttribute("style", "display: flex");
     }
 }
+
+
 
