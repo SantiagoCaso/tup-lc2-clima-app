e8763455b986cdd4b53b2b784e6d199687ea72d2
SantiagoCaso <santiagoignaciocaso@gmail.com>
2022-07-05 11:35:23 -0300

    Solucion de error de datos dobles en la card de consultar clima

--- js/pages/index.js
+++ js/pages/index.js
@@ -38,6 +38,13 @@ const titleCity = document.getElementById("city-title");
 const climaImagen = document.getElementById("img-clima");
 const carta = document.getElementById("carta-clima");
 const loading = document.getElementById("div-carga");
+let nombre;
+let icono;
+let temperatura;
+let sensacionTermica;
+let humedad;
+let velocidadDelViento;
+let presion;
 
 function fetchAPI() {
     loading.removeAttribute("style");
@@ -48,54 +55,97 @@ function fetchAPI() {
             loading.removeAttribute("style");
             loading.setAttribute("style", "display: none");
             console.log(datos);
-            // City title 
-            let name = datos['name'];
-            let cityName = name;
-            titleCity.innerHTML = cityName;
-            // Imagen de clima 
-            let imgClima = datos.weather[0].icon;
-            let imgCode = "http://openweathermap.org/img/w/" + imgClima + ".png";
-            climaImagen.setAttribute("src", imgCode);
-            // Temperatura 
-            let mainTemp = datos['main']['temp'];
-            let temperatura = mainTemp;
-            let p_temp = document.createElement('p'); 
-            p_temp.textContent = 'Temperatura: ' + temperatura + "°";
-            // Sensación termica
-            let mainFeelsLike = datos['main']['feels_like'];
-            let sensacion_termica = mainFeelsLike;
-            let p_feels_like = document.createElement('p');
-            p_feels_like.textContent = 'Sensación termica: ' + sensacion_termica + "°";
-            // Humedad
-            let mainHumidity = datos['main']['humidity'];
-            let humedad = mainHumidity;
-            let p_humidity = document.createElement('p');
-            p_humidity.textContent = 'Humedad: ' + humedad + '%'; 
-            // Velocidad de viento
-            let mainWindSpeed = datos['wind']['speed'];
-            let viento = mainWindSpeed;
-            let p_wind_speed = document.createElement('p');
-            p_wind_speed.textContent = 'Velocidad del viento: ' + viento + 'km/h';
-            // Presión
-            let mainPressure = datos['main']['pressure'];
-            let presion = mainPressure;
-            let p_pressure = document.createElement('p');
-            p_pressure.textContent = "Presión: " + presion + " P";
-
-            carta.appendChild(p_temp);
-            carta.appendChild(p_feels_like);
-            carta.appendChild(p_humidity);
-            carta.appendChild(p_wind_speed);
-            carta.appendChild(p_pressure);
-            carta.removeAttribute("style");
-            carta.setAttribute("style", "display: block");
+            removesChildsCard();
+            createCard(datos);
         })
     .catch(error => alert("La API no funcionó"))
 }
 
-// function createCard(){
-//     fetchAPI();
-// }
+function createCard(datos){
+    // City title 
+    let name = datos['name'];
+    nombre = name;
+    titleCity.innerHTML = nombre;
+    // Imagen de clima 
+    let imgClima = datos.weather[0].icon;
+    let imgCode = "http://openweathermap.org/img/w/" + imgClima + ".png";
+    icono = imgCode;
+    climaImagen.setAttribute("src", icono);
+    // Temperatura 
+    let mainTemp = datos['main']['temp'];
+    let p_temp = document.createElement('p'); 
+    p_temp.setAttribute("id", "temperatura");
+    temperatura = mainTemp;
+    p_temp.textContent = 'Temperatura: ' + temperatura + "°";
+    // Sensación termica
+    let mainFeelsLike = datos['main']['feels_like'];
+    sensacionTermica = mainFeelsLike;
+    let p_feels_like = document.createElement('p');
+    p_feels_like.setAttribute("id", "sensacion_termica");
+    p_feels_like.textContent = 'Sensación termica: ' + sensacionTermica + "°";
+    // Humedad
+    let mainHumidity = datos['main']['humidity'];
+    humedad = mainHumidity;
+    let p_humidity = document.createElement('p');
+    p_humidity.setAttribute("id", "humedad");
+    p_humidity.textContent = 'Humedad: ' + humedad + '%'; 
+    // Velocidad de viento
+    let mainWindSpeed = datos['wind']['speed'];
+    velocidadDelViento = mainWindSpeed;
+    let p_wind_speed = document.createElement('p');
+    p_wind_speed.setAttribute("id", "velocidad_del_viento");
+    p_wind_speed.textContent = 'Velocidad del viento: ' + velocidadDelViento + 'km/h';
+    // Presión
+    let mainPressure = datos['main']['pressure'];
+    presion = mainPressure;
+    let p_pressure = document.createElement('p');
+    p_pressure.setAttribute("id", "presion");
+    p_pressure.textContent = "Presión: " + presion + " P";
+
+    carta.appendChild(p_temp);
+    carta.appendChild(p_feels_like);
+    carta.appendChild(p_humidity);
+    carta.appendChild(p_wind_speed);
+    carta.appendChild(p_pressure);
+    carta.removeAttribute("style");
+    carta.setAttribute("style", "display: block");
+}
+
+function removesChildsCard() {
+    let pTemp = document.getElementById("temperatura");
+    let pFeelsLike = document.getElementById("sensacion_termica");
+    let pHumidity = document.getElementById("humedad");
+    let pWindSpeed = document.getElementById("velocidad_del_viento");
+    let pPressure = document.getElementById("presion");
+    if (pTemp != null) { 
+        carta.removeChild(pTemp);
+        carta.removeChild(pFeelsLike);
+        carta.removeChild(pHumidity);
+        carta.removeChild(pWindSpeed);
+        carta.removeChild(pPressure);
+    }
+}
+
 
 btnConsultar.addEventListener("click", fetchAPI);
 selctCities.addEventListener("click", showAlert);
+
+
+// async function asyncAwaitFetchAPI(){
+//     loading.removeAttribute("style");
+//     loading.setAttribute("style", "display: block");
+//     try {
+//         let response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+selctCities.value+'&appid=c4d912fb3ee953d0623afe3268b33688&units=metric&lang=es')
+//         console.log(response);
+//         response => response.json()
+//         (datos => {
+//             loading.removeAttribute("style");
+//             loading.setAttribute("style", "display: none");
+//             console.log(datos);
+//             removesChildsCard();
+//             createCard(datos);
+//         })
+//     } catch (error) {
+        
+//     }
+// }
